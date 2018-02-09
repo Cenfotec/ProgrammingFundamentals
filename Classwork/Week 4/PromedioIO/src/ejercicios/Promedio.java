f2121ffa5bed5e5059371aac50c04934c6dc298d
Pablo Bonilla <pbonilla29@hotmail.com>
2018-02-08 19:59:37 -0600

    Added "PromedioIO" to Week 4

---
+++ Classwork/Week 4/PromedioIO/src/ejercicios/Promedio.java
@@ -0,0 +1,38 @@
+// Comentario en una linea
+
+/*
+Comentario
+en diferentes
+lineas
+ */
+
+/**
+ * Comentario
+ * automatico
+ * en diferentes
+ * lineas
+ */
+
+package ejercicios;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import javax.swing.*;
+
+public class Promedio {
+
+    public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static PrintStream out = System.out;
+
+    public static void main(String[] args) throws IOException {
+        double numUno = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite el primer numero"));
+        double numDos = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite el segundo numero"));
+        String userInput = JOptionPane.showInputDialog(null, "Digite el tercer numero");
+        double numTres = Double.parseDouble(userInput);
+        double resultado = (numUno + numDos + numTres) / 3;
+        out.println("El promedio es: " + resultado);
+    }
+
+}
