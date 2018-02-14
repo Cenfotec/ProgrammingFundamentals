4657ad2ec2f6f36dd802729b5941aab6d0ebf94a
Pablo Bonilla <pbonilla29@hotmail.com>
2018-02-14 14:14:36 -0800

    Fixed CalcularCantidadDinero

--- Classwork/Week 5/CalcularCantidadDinero/src/ejercicios/Main.java
+++ Classwork/Week 5/CalcularCantidadDinero/src/ejercicios/Main.java
@@ -1,16 +1,15 @@
-// Comentario en una linea
-
-/*
-Comentario
-en diferentes
-lineas
- */
-
 /**
- * Comentario
- * automatico
- * en diferentes
- * lineas
+ * Nombre del Programa: Calcular la cantidad de dinero
+ *                      a partir de un interes.
+ *
+ * Descripcion: Calcular cuánto dinero tiene una persona después de N años, dado i como
+ *              tasa de interés y p como el monto inicial. (valor = p*(1+i)N).
+ *
+ * Fecha de Creacion: 13/2/2018
+ * Autor: Pablo Bonilla G.
+ *
+ * Fecha de Modificacion: 13/2/2018
+ * Modificado Por: Pablo Bonilla G.
  */
 
 package ejercicios;
@@ -19,20 +18,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import javax.swing.*;
 
 public class Main {
 
-    public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    // Declaracion de los flujos de entrada y salida
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static PrintStream out = System.out;
 
     public static void main(String[] args) throws IOException {
-        double numUno = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite el primer numero"));
-        double numDos = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite el segundo numero"));
-        String userInput = JOptionPane.showInputDialog(null, "Digite el tercer numero");
-        double numTres = Double.parseDouble(userInput);
-        double resultado = (numUno + numDos + numTres) / 3;
-        out.println("El promedio es: " + resultado);
-    }
+        out.print("Cantidad de años: ");
+        int years = Integer.parseInt(br.readLine());
 
+        out.print("Tasa de interes: ");
+        double tasaInteres = Double.parseDouble(br.readLine());
+
+        out.print("Monto inicial: ");
+        double montoInicial = Double.parseDouble(br.readLine());
+
+        double valor = montoInicial*Math.pow(1+tasaInteres, years);
+        System.out.println("La persona tendria: " + valor);
+    }
 }
