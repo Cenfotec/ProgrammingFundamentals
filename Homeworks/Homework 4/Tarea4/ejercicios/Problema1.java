a0731df570955020b54ab8eeb0a900b6d11859ff
Pablo Bonilla <pbonilla29@hotmail.com>
2018-02-16 22:01:21 -0800

    Added Homework 4
    Implemented Problem 1, 2, 3, 4 and 5

---
+++ Homeworks/Homework 4/Tarea4/ejercicios/Problema1.java
@@ -0,0 +1,22 @@
+package ejercicios;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+public class Problema1 {
+
+    public static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static final PrintStream out = System.out;
+    public static final int TELA_POR_PERSONA = 3;
+
+    public static void main(String[] args) throws IOException {
+        out.println("¿Cuantas personas quieren un vestido?");
+
+        int personas = Integer.parseInt(br.readLine());
+        int totalTela = TELA_POR_PERSONA*personas;
+
+        out.println("Se necesitan " + totalTela + "m de tela.");
+    }
+}
---
+++ Homeworks/Homework 4/Tarea4/ejercicios/Problema2.java
@@ -0,0 +1,23 @@
+package ejercicios;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+public class Problema2 {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static PrintStream out = System.out;
+
+    public static void main(String[] args) throws IOException {
+        out.print("Distancia en metros: ");
+        float distancia = Float.parseFloat(br.readLine());
+
+        out.print("Tiempo en segundos: ");
+        float tiempo = Float.parseFloat(br.readLine());
+
+        float velocidad = distancia/tiempo;
+        out.printf("El vehículo viaja a %.1f m/s", velocidad);
+    }
+}
