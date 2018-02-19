eb775bf62019994561b3311aec8665b0c81fc325
Pablo Bonilla <pbonilla29@hotmail.com>
2018-02-19 09:03:00 -0800

    Improved Problem 6

--- Homeworks/Homework 4/Tarea4/ejercicios/Problem6.java
+++ Homeworks/Homework 4/Tarea4/ejercicios/Problem6.java
@@ -13,19 +13,19 @@ public class Problem6 {
     public static void main(String[] args) throws IOException {
         out.print("Monto total de colones: ");
         int colones = Integer.parseInt(br.readLine());
-		
+
         int[] m = new int[4];
-        for (int i = 0, j, w; i <= 3; i++) {
+        for (int i = 0, j; i <= 3; i++) {
             j = (i == 0) ? 50 : (i == 1) ? 20 : (i == 2) ? 10 : 5;
             m[i] = colones / j;
-            w = (i == 0) ? m[0] : (i == 1) ? m[1] : (i == 2) ? m[2] : m[3];
-            colones -= j * w;
+            colones -= j * m[i];
         }
-		
-        out.println("Monedas de 50: " + m[0]);
-        out.println("Monedas de 20: " + m[1]);
-        out.println("Monedas de 10: " + m[2]);
-        out.println("Monedas de 5: " + m[3]);
-        out.println("Sobran: " + colones + " colones");
+
+        out.println("Monedas de 50: " + m[0] +
+                  "\nMonedas de 20: " + m[1] +
+                  "\nMonedas de 10: " + m[2] +
+                  "\nMonedas de 5: " + m[3] +
+                  "\nSobran: " + colones + " colones");
+
     }
 }
